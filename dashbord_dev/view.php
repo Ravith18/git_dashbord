b9056124d4e5db5c09afbd8070a8b098ee2ad908
Phon Ravith <phonravith18@gmail.com>
2026-05-10 16:49:13 +0700

    add the view.php to the folder/

---
+++ dashbord_dev/view.php
@@ -0,0 +1,16 @@
+<nav class="topnav">
+  <div class="nav-logo">Dashboard<span>⬡</span></div>
+  <div class="nav-subtitle">XXX</div>
+  <div class="nav-tabs">
+    <div class="nav-tab on"   onclick="goPage('journal',this)">Journal</div>
+    <div class="nav-tab"      onclick="goPage('discipline',this)">Discipline</div>
+    <div class="nav-tab"      onclick="goPage('dashboard',this)">Dashboard</div>
+    <div class="nav-tab"      onclick="goPage('accounts',this)">Accounts</div>
+    <div class="nav-tab"      onclick="goPage('leakmap',this)">Leak Map</div>
+  </div>
+  <div class="nav-spacer"></div>
+  <div class="nav-actions">
+    <button class="btn gold" onclick="openPreSession()">+ New Session Day</button>
+    <button class="btn"      onclick="show('mAcc')">+ Account</button>
+  </div>
+</nav>
